41fa2f4ea68ffd453eeef0e02c855ed8a07d6ec1
Aaryan Bhurtel <aaryan.bhurtel@sifal.deerwalk.edu.np>
2026-08-15 05:47:28 +0545

    Data Cleaning: Checked for null values and verified using ICC points table if they were consistent, since the null vaules didnt exist, it implies that all the matches had results

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ cwc = pd.read_csv("CWC2023.csv")
 #before cleaning the data we hvae 48 entries including the null values if there are any
 print(cwc.info())
 
+
+#to see if there is any null values
+
+print(cwc.isnull().sum()) 
+# there are no null values implying that all the 48 rows contains matches with winners and result and no game had ended on N/R
+# further checking was done via points table and the results were consistent with the data in the csv file
+
